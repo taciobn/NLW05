50f513362f6dba9f0ee133dd33b3ec3313a5fb62
Tácio Barreto <taciobarreto@hotmail.com>
2021-05-23 21:10:51 -0300

    nem sei

---
+++ Node/public/js/chat.js
@@ -0,0 +1,4 @@
+document.querySelector("#start_chat").addEventListener("click", (event) => {
+    const socket = io();
+
+});
--- Node/src/server.ts
+++ Node/src/server.ts
@@ -1,17 +1,7 @@
-import express, { request, response } from 'express';
+import { http} from "./http";
+import "./websocket/client";
 
-import "./database";
-
-import {routes} from "./routes"
-
-const app = express();
-
-app.use(express.json())
-
-app.use(routes);
-
-
-app.listen(3333, ()=>{console.log("Servers is running on port 3333");
+http.listen(3333, ()=>{console.log("Servers is running on port 3333");
 });
 
 
